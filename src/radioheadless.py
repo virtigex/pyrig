08aa2f80cd2d2db4a68ff292c84a01fca3d94827
Steve Austin <trans.medium@gmail.com>
2025-03-27 17:23:41 -0700

    WIP radioheadless.py and records audio

--- src/radioheadless.py
+++ src/radioheadless.py
@@ -4,68 +4,21 @@ import numpy as np
 import sys
 
 DIR = 'C:\\Users\\Steve\\Documents\\radioheadless'
-SAMPLE_RATE = 8000
-BUFFER_SIZE = 80 # 10 ms per bufffer
-RECLEN = 300 * 80 # five minutes
+SAMPLE_RATE = 48000
+BUFFER_SIZE = 480 # 10 ms per bufffer
+RECLEN = 3000 # five minutes
 
-class Frame:
+from ringbuffer import RingBuffer, RingListener
+
+class Listener(RingListener):
     def __init__(self):
-        self.data = np.empty((BUFFER_SIZE, 1), dtype=np.int16)
-        self.pos = 0
-        self.valid = False
+        RingListener.__init__(self)
 
-    def reset(self):
-        self.pos = 0
-        self.power = 0.0
-        self.valid = False
-
-    def fill(self, indata, offset):
-        insamps = indata.shape[0] - offset
-        towrite = BUFFER_SIZE - self.pos
-        if insamps < towrite:
-            for i in range(insamps):
-                self.data[self.pos] = indata[offset + i]
-                self.pos += 1
-            return offset + insamps
-        for i in range(towrite):
-            self.data[self.pos] = indata[offset + i]
-            self.pos += 1
-        power = 0
-        for s in self.data:
-            samp = float(s)
-            power += samp * samp
-        self.power = power
-        self.valid = True
-        return offset + towrite
-
-    def isvalid(self):
-        return self.valid
-
-class RingBuffer:
-
-    def __init__(self, noframes):
-        self.bufs = [Frame() for _ in range(noframes)]
-        self.next = 0
-        self.tail = 0
-
-    def process(self, indata):
-        offset = 0
-        while offset < indata.shape[0]:
-            bin = self.next % len(self.bufs)
-            offset = self.bufs[bin].fill(indata, offset)
-            if self.bufs[bin].isvalid():
-                self.next += 1
-                # bins self.tail to (self.next-1) are valid here
-                self.detect()
-                print(f'valid buf {bin}')
-                bufsize = len(self.bufs)
-                if (self.next % bufsize) == (self.tail % bufsize):
-                    self.bufs[self.tail].reset()
-                    self.tail += 1
-
-    def detect(self):
-        p = [self.bufs[(self.next-i-1) % len(self.bufs)].power for i in range(self.next - self.tail)]
-        print(f'{len(p)}: {p}')
+    def detect(self, bufs, next, tail):
+        p = [bufs[(next-i-1) % len(bufs)].power for i in range(next - tail)]
+        print(*p, sep=',')
+        #print(f'RH: {len(p)}: {p}')
+        return tail
 
 async def collect(dir):
 
@@ -76,24 +29,25 @@ async def collect(dir):
     no_frames = 0
     loop = asyncio.get_event_loop()
     event = asyncio.Event()
-    ringbuf = RingBuffer(RECLEN)
+    listener = Listener()
+    ringbuf = RingBuffer(RECLEN, BUFFER_SIZE, listener)
 
     def callback(indata, frames, time, status):
         nonlocal no_recording
         nonlocal no_frames
         nonlocal ringbuf
         nonlocal running
-        print(f'status: {status}, frames: {frames}, time: {time}, indata: {indata.shape}')
+        #print(f'status: {status}, frames: {frames}, time: {time}, indata: {indata}')
         ringbuf.process(indata)
         #if not running:
-        if no_frames > 1000 or not running:
+        if no_frames > 100 or not running:
             loop.call_soon_threadsafe(event.set)
             raise sd.CallbackStop
         no_frames += 1
-        print(no_frames)
+        #print(no_frames)
 
     try:
-        stream = sd.InputStream(device=None, samplerate=SAMPLE_RATE, channels=1, callback=callback)
+        stream = sd.InputStream(device=None, channels=1, samplerate=48000.0, callback=callback)
         stream.start()
         await event.wait()
     except KeyboardInterrupt:
@@ -108,10 +62,11 @@ async def collect(dir):
     return no_recording
 
 async def main():
-    if len(sys.argv)!= 2:
-        print(f"Usage: {sys.argv[0]} <directory>")
-        sys.exit(1)
-    dir = sys.argv[1]
+    #if len(sys.argv)!= 2:
+    #    print(f"Usage: {sys.argv[0]} <directory>")
+    #    sys.exit(1)
+    #dir = sys.argv[1]
+    dir = 'c\\tmp'
     recorded_audio = await collect(dir)
     print(f"number of frames recorded: {recorded_audio}")
 
---
+++ src/ringbuffer.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class Frame:
+    def __init__(self, framesize):
+        self.framesize = framesize
+        self.data = np.empty((self.framesize, 1), dtype=np.float32)
+        self.pos = 0
+        self.valid = False
+
+    def reset(self):
+        self.pos = 0
+        self.power = 0.0
+        self.valid = False
+
+    def fill(self, indata, offset):
+        insamps = indata.shape[0] - offset
+        towrite = self.framesize - self.pos
+        if insamps < towrite:
+            for i in range(insamps):
+                self.data[self.pos] = indata[offset + i][0]
+                self.pos += 1
+            return offset + insamps
+        for i in range(towrite):
+            self.data[self.pos] = indata[offset + i][0]
+            self.pos += 1
+        power = 0
+        for s in self.data:
+            samp = float(s)
+            power += samp * samp
+        self.power = power
+        self.valid = True
+        return offset + towrite
+
+    def isvalid(self):
+        return self.valid
+
+class RingBuffer:
+
+    def __init__(self, noframes, framesize, listener):
+        self.bufs = [Frame(framesize) for _ in range(noframes)]
+        self.listener = listener
+        self.next = 0
+        self.tail = 0
+
+    def process(self, indata):
+        offset = 0
+        #print(f'shape = {indata.shape}')
+        #print(indata)
+        while offset < indata.shape[0]:
+            bin = self.next % len(self.bufs)
+            offset = self.bufs[bin].fill(indata, offset)
+            if self.bufs[bin].isvalid():
+                self.next += 1
+                # bins self.tail to (self.next-1) are valid here
+                self.tail = self.listener.detect(self.bufs, self.next, self.tail)
+                #print(f'valid buf {bin}')
+                bufsize = len(self.bufs)
+                if (self.next % bufsize) == (self.tail % bufsize):
+                    self.bufs[self.tail].reset()
+                    self.tail += 1
+
+class RingListener:
+    def __init__(self):
+        pass
+
+    def detect(self, bufs, next, tail):
+        p = [bufs[(next-i-1) % len(bufs)].power for i in range(next - tail)]
+        print(f'{len(p)}: {p}')
+        # tail does not move in dummy implemenation
+        return self.tail
